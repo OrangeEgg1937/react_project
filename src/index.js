1dbbf864b0f2fca0aac9a0740698b2a47c0d2656
OrangeEgg1937 <48792791+OrangeEgg1937@users.noreply.github.com>
2023-11-11 12:02:27 +0000

    problem1

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import {
   Link,
 } from 'react-router-dom';
 
-
 class App extends React.Component {
   render() {
     return (
@@ -15,7 +14,8 @@ class App extends React.Component {
         <div>
           <ul>
             <li> <Link to="/">Home</Link> </li>
-            <li> <Link to="/about">About</Link> </li>
+            <li> <Link to="/gallery">Gallery</Link> </li>
+            <li> <Link to="/slideshow">Slideshow</Link> </li>
           </ul>
         </div>
 
@@ -23,7 +23,8 @@ class App extends React.Component {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/slideshow" element={<Slideshow />} />
         </Routes>
       </BrowserRouter>
     );
@@ -36,9 +37,56 @@ class Home extends React.Component {
   }
 }
 
-class About extends React.Component {
+const data = [
+  {filename: "cuhk-2013.jpg", year:2013, remarks: "Sunset over CUHK"}, 
+  {filename: "cuhk-2017.jpg", year:2017, remarks: "Bird's-eye view of CUHK"},
+  {filename: "sci-2013.jpg", year:2013, remarks: "The CUHK Emblem"}, 
+  {filename: "shb-2013.jpg", year:2013, remarks: "The Engineering Buildings"},
+  {filename: "stream-2009.jpg", year:2009, remarks: "Nature hidden in the campus"},
+  ];
+
+class Gallery extends React.Component {
   render() {
-    return <h2>About</h2>;
+    return (
+        <main className="container">
+            {data.map((file,index) => <FileCard i={index} key={index}/>)}
+        </main>
+    );
+  }
+}
+
+class FileCard extends React.Component {
+    
+  constructor(props) {
+      super(props);
+      this.state = { selected: -1 };
+      }
+  
+  handleClick(index, e) {
+      if (this.state.selected !== index) {
+          this.setState({ selected: index });
+        } else {
+          this.setState({ selected: -1 });
+        }
+  }
+
+  render() {
+      let i = this.props.i;
+      return (
+              <div className="card d-inline-block m-2" style={{width:this.state.selected==i ? "100%" : 200}} onClick={(e) => this.handleClick(i, e)}>
+                  <img src={"images/"+data[i].filename} className="w-100" />
+                  <div className="card-body">
+                      <h6 className="card-title">{data[i].filename}</h6>
+                      <p className="card-text">{data[i].year}</p>
+                  </div>
+              </div>
+      );
+  }
+}
+
+class Slideshow extends React.Component {
+  render() {
+    return <h2>Slideshow</h2>;
   }
 }
 
